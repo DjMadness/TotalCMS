056fec22a568a37dd62d1bf056d5b58cf80337e1
DjMadness <DjMadness@totalirc.net>
2016-02-22 16:27:28 +0100

    renamed news.class.php file to article.class.php and added methods (no data yet)

---
+++ include/class/article.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Article extends D {
+    public $Database;
+    public function __construct() {
+        parent::__construct();
+    }
+    private function Placeholder() {
+        $this->Database = new Database();
+    }
+    public function AddArticle($usersId, $title, $text) {
+        
+    }
+    public function DelArticle($articleId) {
+        
+    }
+    public function EditArticle() {
+        
+    }
+    public function ViewArticle($articleId) {
+        
+    }
+    public function GetArticle($usersId, $title, $text) {
+        
+    }
+    public function ListArticles() {
+        
+    }
+    public function VoteArticle($articleId, $vote) {
+        
+    }
+    public function GetComments($articleId) {
+        
+    }
+    public function Comment($articleId, $usersId, $comment) {
+        
+    }
+    public function DelComment($commentId) {
+        
+    }
+    public function EditComment($commentId, $comment) {
+        
+    }
+    public function VoteComment($commentId, $vote) {
+        
+    }
+}

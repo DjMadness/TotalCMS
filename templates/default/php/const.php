12f4f5be949e2b6ce91032331008861f551e44c5
DjMadness <DjMadness@totalirc.net>
2016-02-22 03:34:12 +0100

    added a constant file for the default theme.

---
+++ templates/default/php/const.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Basicly this file helps define the standards of this theme, it helps make it easier to load the standards for the theme
+ * such as image sizes etc (for file uploads, such as icons, news articles etc, and will always base it on the original filename
+ */
+define(NAVI_ICON_WIDTH, 32); // 32 pixels width
+define(NAVI_ICON_HEIGHT, 32); // 32 pixels height
+?>

255674f900e12b09b811a2b44f294c2c2972836a
DjMadness <DjMadness@totalirc.net>
2016-02-22 02:38:23 +0100

    added initial navigation class

---
+++ include/class/navigation.class.php
@@ -0,0 +1,22 @@
+<?php
+class Navigation extends D {
+    public $Database;
+    public function __construct() {
+        parent::__construct();
+    }
+    private function Placeholder() {
+        $this->Database = new Database();
+    }
+    public function AddNav($name, $url, $position,  $image = null) {
+        
+    }
+    public function DelNav($navid) {
+        
+    }
+    public function ListNav() {
+        
+    }
+    public function ReorderNav($navid1, $navid2) {
+        
+    }
+}

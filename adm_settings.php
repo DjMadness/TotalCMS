ccd0c31f691fbbbe2dcee5a4e24929430ccdf6f4
DjMadness <DjMadness@totalirc.net>
2016-02-23 22:25:47 +0100

    renamed settings to adm_settings and adjusted source to reflect this.

--- adm_settings.php
+++ adm_settings.php
@@ -6,24 +6,24 @@ include('include/header.php');
 function settings_list() {
     global $Smarty;
     $Smarty->assign('settings_list', Settings::S()->ListSettings());
-    display('settings/list.tpl');
+    display('adm_settings/list.tpl');
 }
 function settings_error($settings_error) {
     global $Smarty;
     $Smarty->assign('settings_error', $settings_error);
-    display('settings/error.tpl');
+    display('adm_settings/error.tpl');
 }
 if (filter_input(INPUT_GET, 'do')) {
     switch (filter_input(INPUT_GET, 'do')) {
         case 'add':
-            display('settings/add.tpl');
+            display('adm_settings/add.tpl');
             break;
         case 'edit':
             if (filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT)) {
                 $settings_list = Settings::S()->GetById(filter_input(INPUT_GET, 'id'));
                 if ($settings_list != false) {
                     $Smarty->assign('settings_list', $settings_list);
-                    display('settings/edit.tpl');
+                    display('adm_settings/edit.tpl');
                 } else settings_error('Setting does not exist');
             } else settings_list();
             break;
@@ -40,7 +40,7 @@ if (filter_input(INPUT_GET, 'do')) {
                 $settings_list = Settings::S()->GetById(filter_input(INPUT_GET, 'id'));
                 if ($settings_list != false) {
                     $Smarty->assign('settings_list', $settings_list);
-                    display('settings/delete.tpl');
+                    display('adm_settings/delete.tpl');
                 } else settings_error('Setting does not exist');
             } else settings_list();
             break;

ace81dacc428809d1a9f3ef5ad8d6a1c6d0a001d
DjMadness <DjMadness@totalirc.net>
2016-02-24 01:35:56 +0100

    completed login php

--- login.php
+++ login.php
@@ -2,4 +2,15 @@
 
 session_start();
 include('include/header.php');
-display('login.tpl');
+if (filter_input(INPUT_POST, 'login_submit')) {
+    if(filter_input(INPUT_POST, 'login_username')) $login_username = filter_input(INPUT_POST, 'login_username', FILTER_SANITIZE_STRING);
+    if(filter_input(INPUT_POST, 'login_password')) $login_password = filter_input(INPUT_POST, 'login_password', FILTER_SANITIZE_STRING);
+    if((isset($login_username)) && (isset($login_password))) {
+        if(User::S()->Login($login_username, $login_password) == true)
+            header('location: index.php');
+        else {
+            $Smarty->assign('login_error', 'Invalid user/pass combination');
+            display('login.tpl');
+        }
+    }
+} else display('login.tpl');
